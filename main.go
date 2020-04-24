a2e04dead06ff5a880d5d1c42562a74caac0d706
Ethan Hampton <ethohampton@gmail.com>
2020-04-23 20:11:52 -0700

    A bit more cleanup

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func main() {
 	log.SetFlags(log.Ltime | log.Lmicroseconds) //TODO remove time code from logging
 	ttl := flag.Int("ttl", defaultTTL, "Set TTL of ping requests")
 	interval := flag.Int("interval", defaultInterval, "Set interval between ping requests in seconds")
-	debug := flag.Bool("debug", false, "TODO just for testing")
 	flag.Parse()
 
 	rawDestination := flag.Arg(0)
@@ -77,23 +76,24 @@ func main() {
 		c.IPv4PacketConn().SetTTL(*ttl)
 	}
 
-	//TODO TODO FOR TESTING
-	if *debug {
-		return
-	}
-
 	//start of actual technical stuff TODO
 	listenOutput := make(chan ReturnData)
 	go receive(c, isIPv6, listenOutput)
 
 	seq := 1
+	messagesRecived := 0
 	//have to work around weird golang time syntax for multipling an interval by a constants
 	tick := time.Tick(time.Duration(*interval) * time.Second)
 
 	for {
 		select {
 		case data := <-listenOutput:
-			log.Printf("Time between ping: %d", data.delay/nanoToMilli)
+			if data.ID == -1 {
+				log.Println("Time to live exceeded")
+			} else {
+				log.Printf("Repy received: icmp_seq=%d time=%0.1f ms", data.ID, data.delay/nanoToMilli)
+			}
+			messagesRecived++
 			break
 		case <-tick:
 			send(c, &dest, seq, isIPv6)
@@ -104,36 +104,43 @@ func main() {
 }
 
 func receive(c *icmp.PacketConn, isIPv6 bool, output chan ReturnData) {
-	log.Println("Started receiver")
-
+	var proto int
+	if isIPv6 {
+		proto = ipv6MagicNumber
+	} else {
+		proto = ipv4MagicNumber
+	}
 	rb := make([]byte, maxPacketSize)
+
 	for {
-		//read
-		log.Println("Listening")
-		n, peer, err := c.ReadFrom(rb)
-		if err != nil {
-			log.Println("Can't read from connection")
-			log.Fatal(err)
-		}
-
-		rm, err := icmp.ParseMessage(ipv4MagicNumber, rb[:n])
+		n, _, err := c.ReadFrom(rb)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//Reciving pretty much the same message so can case body to type echo in both cases which works fine //TODO TESTTHIS
+		rm, err := icmp.ParseMessage(proto, rb[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//Reciving pretty much the same message for ipv4 or ipv6
+		//so can cast body to type Echo in both cases which works fine
 		switch rm.Type {
 		case ipv4.ICMPTypeEchoReply:
 			fallthrough
 		case ipv6.ICMPTypeEchoReply:
-			log.Printf("got reflection from %v\n", peer)
-
 			returnBody, _ := rm.Body.(*icmp.Echo)
 			returnTimeNano := (time.Now().UnixNano() - int64(binary.LittleEndian.Uint64(returnBody.Data)))
-			output <- ReturnData{ID: returnBody.ID, delay: returnTimeNano}
+			output <- ReturnData{ID: returnBody.Seq, delay: returnTimeNano}
+
+		//handle time exceeded messages
+		case ipv4.ICMPTypeTimeExceeded:
+			fallthrough
+		case ipv6.ICMPTypeTimeExceeded:
+			output <- ReturnData{ID: -1}
 		default:
-			log.Printf("got %+v; want echo reply", rm)
-		} //TODO accept expired requests
+			//ignore other messages
+		}
 	}
 }
 
@@ -143,12 +150,19 @@ func send(c *icmp.PacketConn, dest net.Addr, seqNum int, isIPv6 bool) {
 	body := make([]byte, 8)
 	binary.LittleEndian.PutUint64(body, uint64(t))
 
+	var mesType icmp.Type
+	if isIPv6 {
+		mesType = ipv6.ICMPTypeEchoRequest
+	} else {
+		mesType = ipv4.ICMPTypeEcho
+	}
+
 	wm := icmp.Message{
-		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Type: mesType, Code: 0,
 		Body: &icmp.Echo{
-			ID: os.Getpid() & 0xffff, Seq: seqNum,
-			Data: body, //[]byte("TESTING"), //TODO use time sent here, this would help once we got it back in order to determine what time it was sent
-			//Also would make it easier to run asynconously as it could just present packets as we get them. Also include sequence #s for tracking !
+			ID:   os.Getpid() & 0xffff,
+			Seq:  seqNum,
+			Data: body,
 		},
 	}
 	wb, err := wm.Marshal(nil)
